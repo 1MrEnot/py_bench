c034877f47854305710a6be062da5cfdf5608412
meremeev <maxim9236@yandex.ru>
2024-02-06 23:58:32 +0300

    Extend .girignore and enrich setup

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ name = 'py_bench'
 setup(
     name=name,
     version=open(f'./{name}/_version.py').readline().split('=')[-1].strip(' \n"\''),
+    description='py_bench: python benchmark tool',
+    long_description='py_bench: python benchmark tool',
+    author='1MrEnot',
+    license='MIT License',
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
 )
